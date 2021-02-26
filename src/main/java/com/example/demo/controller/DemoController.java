2209fe32ff6cb67cf1ebf2e1b6ed29690e8d4991
mint <mint@mac.local>
2021-02-26 21:58:31 +0200

    init

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String getGreeting(){
+        return LocalDateTime.now().toString();
+    }
+
+
+
+}
